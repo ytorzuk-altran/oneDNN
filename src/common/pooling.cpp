c796bdb9a84905da48bffeaaaa6603f9e0bb306f
Alexander Peskov <alexander.peskov@intel.com>
2018-03-05 16:58:49 +0300

    Pooling pads like Caffe
    Signed-off-by: Alexander Peskov <alexander.peskov@intel.com>
    
    Signed-off-by: Alexander Peskov <alexander.peskov@intel.com>

--- src/common/pooling.cpp
+++ src/common/pooling.cpp
@@ -110,13 +110,13 @@ status_t pooling_desc_init(pooling_desc_type *pool_desc, prop_kind_t prop_kind,
 
         if ((src - ker_range + pad_l + pad_r) / str + 1 != dst)
             return invalid_arguments;
-
-        // It's not allowed for pooling window to be totally placed outside
-        // of real source domain for pooling_avg_exclude_padding algorithm
-        // due to 0 / 0 ambiguity
-        if (alg_kind == pooling_avg_exclude_padding
-                && !(pad_l < ker_range && pad_r < ker_range && dil < src))
-            return invalid_arguments;
+// The check is disabled in order to support old behavior
+//        // It's not allowed for pooling window to be totally placed outside
+//        // of real source domain for pooling_avg_exclude_padding algorithm
+//        // due to 0 / 0 ambiguity
+//        if (alg_kind == pooling_avg_exclude_padding
+//                && !(pad_l < ker_range && pad_r < ker_range && dil < src))
+//            return invalid_arguments;
     }
 
     *pool_desc = pd;
