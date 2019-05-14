d570caab932c6647112184344bbe36a2c0cbef60
test <alexey.varyzgin@intel.com>
2019-05-14 17:19:20 +0300

    TBB_AUTO was enabled

--- src/common/dnnl_thread_parallel_nd.hpp
+++ src/common/dnnl_thread_parallel_nd.hpp
@@ -106,6 +106,9 @@ void parallel(int nthr, F f) {
 #endif
             },
             tbb::static_partitioner());
+#elif DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_TBB_AUTO
+    tbb::parallel_for(
+            0, nthr, [&](int ithr) { f(ithr, nthr); });
 #elif DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_THREADPOOL
     using namespace dnnl::impl::threadpool_utils;
     dnnl::threadpool_interop::threadpool_iface *tp = get_active_threadpool();
@@ -495,6 +498,7 @@ void parallel_nd_in_omp(Args &&... args) {
     for_nd(omp_get_thread_num(), omp_get_num_threads(),
             utils::forward<Args>(args)...);
 #elif (DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_TBB \
+        || DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_TBB_AUTO \
         || DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_THREADPOOL)
     assert(!"parallel_nd_in_omp() is not supported by this DNNL_CPU_RUNTIME");
 #endif
