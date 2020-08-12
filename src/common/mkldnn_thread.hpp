b73474c80c21ae170b112803a1fc315e1549bdab
mandrono <maxim.andronov@intel.com>
2020-08-12 20:50:42 +0300

    [MSVC] Enabling SIMD functionality for VS2019

--- src/common/mkldnn_thread.hpp
+++ src/common/mkldnn_thread.hpp
@@ -77,8 +77,11 @@ inline void mkldnn_thr_barrier() { assert(!"no barrier in TBB"); }
 
 #endif
 
-/* MSVC still supports omp 2.0 only */
-#if defined(_MSC_VER) && !defined(__clang__) && !defined(__INTEL_COMPILER)
+/* MSVC still supports omp 2.0 only,
+   however VS2019 also now offers SIMD functionality 
+   with the -openmp:experimental compilation switch that enables additional OpenMP features
+   not available when using the -openmp switch */
+#if defined(_MSC_VER) && (_MSC_VER < 1900) && !defined(__clang__) && !defined(__INTEL_COMPILER)
 #   define collapse(x)
 #   define PRAGMA_OMP_SIMD(...)
 #else
