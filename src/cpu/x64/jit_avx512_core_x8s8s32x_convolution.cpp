3fe974f77eebaf3bc4e3afe5bf1c307a62e5294e
dmitrygo <dmitry.gorokhov@intel.com>
2020-09-24 23:56:52 +0300

    Introduced Depthwise and Quantization post ops
    Primitives that supports new post ops:
    - Jit Convolutions (FP32,BF16,INT8)
    - Jit,Ref Pooling (INT8)

--- src/cpu/x64/jit_avx512_core_x8s8s32x_convolution.cpp
+++ src/cpu/x64/jit_avx512_core_x8s8s32x_convolution.cpp
@@ -152,6 +152,8 @@ status_t jit_avx512_core_x8s8s32x_convolution_fwd_t::execute_forward_1d(
             p.oc_l_off = (g * jcp.nb_oc + ocb) * jcp.oc_block;
             p.post_ops_binary_rhs_arg_vec = post_ops_binary_rhs_arg_vec.data();
             p.dst_orig = dst;
+            p.oc_off = g_oc * sizeof(float);
+
             (*kernel_)(&p);
 
             ++start;
@@ -329,6 +331,8 @@ status_t jit_avx512_core_x8s8s32x_convolution_fwd_t::execute_forward_2d(
                     p.post_ops_binary_rhs_arg_vec
                             = post_ops_binary_rhs_arg_vec.data();
                     p.dst_orig = dst;
+                    p.oc_off = g_oc * sizeof(float);
+
                     (*kernel_)(&p);
 
                     src_w += src_h_stride * jcp.stride_h;
@@ -474,6 +478,7 @@ status_t jit_avx512_core_x8s8s32x_convolution_fwd_t::execute_forward_2d_dw(
                 p.post_ops_binary_rhs_arg_vec
                         = post_ops_binary_rhs_arg_vec.data();
                 p.dst_orig = dst;
+                p.oc_off = g * sizeof(float);
 
                 (*kernel_)(&p);
             });
@@ -653,6 +658,8 @@ status_t jit_avx512_core_x8s8s32x_convolution_fwd_t::execute_forward_3d(
                     p.post_ops_binary_rhs_arg_vec
                             = post_ops_binary_rhs_arg_vec.data();
                     p.dst_orig = dst;
+                    p.oc_off = g_oc * sizeof(float);
+
                     (*kernel_)(&p);
 
                     src_w += src_h_stride * jcp.stride_h;
