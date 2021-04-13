9db5241c30232f27fa65274ae1812f00e49c9de8
alexey-varyzgin <alexey.varyzgin@intel.com>
2021-04-13 09:50:06 +0300

    An existance of post ops flag was fixed

--- src/cpu/x64/jit_gemm_inner_product_utils.cpp
+++ src/cpu/x64/jit_gemm_inner_product_utils.cpp
@@ -765,8 +765,7 @@ void jit_pp_kernel_t<acc_type, dst_type>::generate() {
     // at least 2 blocks of mb within vlen
     bool dim_restrict = !this->runtime_oc() && !this->runtime_mb()
             && (this->OC_ <= vlen / 2) && (this->MB_ >= vlen);
-    bool supported_postops = this->do_scale_ || this->do_eltwise_
-            || this->do_binary_ || this->do_sum_ || this->do_dst_zero_points_;
+    bool supported_postops = this->do_scale_ || this->do_binary_ || this->do_dst_zero_points_ || (this->post_ops_.len() > 0);
 
     if (this->do_bias() && !supported_postops && dim_restrict
             && this->has_trivial_mb_stride()) {
