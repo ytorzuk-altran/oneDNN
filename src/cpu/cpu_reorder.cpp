8840c3faf6c4e1131c5408e8d6795093d4f4a815
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-07-26 10:16:36 +0300

    Fix for Intel compiler (#63)

--- src/cpu/cpu_reorder.cpp
+++ src/cpu/cpu_reorder.cpp
@@ -133,7 +133,7 @@ using impl_list_map_t = std::map<reorder_impl_key_t, std::vector<rpd_create_f>>;
 /* regular reorders */
 #ifdef __INTEL_COMPILER
 /* direct copy for icc, which is faster than jitted code */
-#define REG_FAST_DIRECT_COPY_COMMA(sdt, ddt) REG_SR_DIRECT_COPY(sdt, ddt),
+#define REG_FAST_DIRECT_COPY_COMMA(sdt, ddt) REG_SR_DIRECT_COPY(sdt, ddt)
 #else
 #define REG_FAST_DIRECT_COPY_COMMA(sdt, ddt)
 #endif
