cf5295181eea5b2034ca50a5f8eae6089e2b4582
dmitrygo <dmitry.gorokhov@intel.com>
2021-08-24 17:13:45 +0300

    Enabled AMX InnerProduct

--- src/cpu/cpu_inner_product_list.cpp
+++ src/cpu/cpu_inner_product_list.cpp
@@ -36,9 +36,9 @@ using namespace dnnl::impl::data_type;
 // clang-format off
 const impl_list_item_t impl_list[] = {
         /* f32 */
-//        CPU_INSTANCE_X64(brgemm_inner_product_fwd_t<avx512_core>)
-//        CPU_INSTANCE_X64(brgemm_inner_product_bwd_data_t<avx512_core>)
-//        CPU_INSTANCE_X64(brgemm_inner_product_bwd_weights_t<avx512_core>)
+        CPU_INSTANCE_X64(brgemm_inner_product_fwd_t<avx512_core>)
+        CPU_INSTANCE_X64(brgemm_inner_product_bwd_data_t<avx512_core>)
+        CPU_INSTANCE_X64(brgemm_inner_product_bwd_weights_t<avx512_core>)
         CPU_INSTANCE(gemm_inner_product_fwd_t<f32>)
         CPU_INSTANCE(gemm_inner_product_bwd_data_t<f32>)
         CPU_INSTANCE(gemm_inner_product_bwd_weights_t<f32>)
@@ -61,8 +61,8 @@ const impl_list_item_t impl_list[] = {
         CPU_INSTANCE(ref_inner_product_fwd_t<bf16, bf16, bf16, f32>)
         CPU_INSTANCE(ref_inner_product_fwd_t<bf16, bf16, f32, f32>)
         /* int */
-//        CPU_INSTANCE_X64(brgemm_inner_product_fwd_t<avx512_core_bf16_amx_int8>)
-//        CPU_INSTANCE_X64(brgemm_inner_product_fwd_t<avx512_core_vnni>)
+        CPU_INSTANCE_X64(brgemm_inner_product_fwd_t<avx512_core_bf16_amx_int8>)
+        CPU_INSTANCE_X64(brgemm_inner_product_fwd_t<avx512_core_vnni>)
         CPU_INSTANCE(gemm_x8s8s32x_inner_product_fwd_t<u8, u8>)
         CPU_INSTANCE(gemm_x8s8s32x_inner_product_fwd_t<u8, s8>)
         CPU_INSTANCE(gemm_x8s8s32x_inner_product_fwd_t<u8, s32>)
