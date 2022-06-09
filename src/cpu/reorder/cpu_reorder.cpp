6a05aaf20c0600102e6bd08877fdd01ac9b17510
Luo Cheng <cheng.luo@intel.com>
2022-06-09 12:20:21 +0800

    Fix possible data race when accessing global reorder list (#129)
    Co-authored-by: Egor Duplensky <egor.duplenskii@intel>

--- src/cpu/reorder/cpu_reorder.cpp
+++ src/cpu/reorder/cpu_reorder.cpp
@@ -22,7 +22,7 @@ namespace impl {
 namespace cpu {
 
 /* regular reorders */
-static std::map<reorder_impl_key_t, const void *> &regular_impl_list_map() {
+const static std::map<reorder_impl_key_t, const void *> &regular_impl_list_map() {
     static std::map<reorder_impl_key_t, const void *> the_map = {
             {{f32, bf16, 0}, &regular_f32_bf16_impl_list_map()},
             {{f32, f16, 0}, &regular_f32_f16_impl_list_map()},
@@ -42,7 +42,7 @@ static std::map<reorder_impl_key_t, const void *> &regular_impl_list_map() {
 }
 
 /* conv reorders w/ compensation */
-static std::map<reorder_impl_key_t, const void *> &comp_s8s8_impl_list_map() {
+const static std::map<reorder_impl_key_t, const void *> &comp_s8s8_impl_list_map() {
     static std::map<reorder_impl_key_t, const void *> the_map = {
             {{f32, s8, 0}, &comp_f32_s8_impl_list_map()},
             {{bf16, s8, 0}, &comp_bf16_s8_impl_list_map()},
@@ -57,16 +57,18 @@ const impl_list_item_t *cpu_engine_impl_list_t::get_reorder_implementation_list(
     const bool do_comp_s8s8 = dst_md->extra.flags
             & (memory_extra_flags::compensation_conv_s8s8
                     | memory_extra_flags::compensation_conv_asymmetric_src);
-    auto &map = do_comp_s8s8 ? comp_s8s8_impl_list_map()
-                             : regular_impl_list_map();
-    const impl_list_map_t *p_impl_list = (const impl_list_map_t *)map[dt_pair];
+    const auto &map = do_comp_s8s8 ? comp_s8s8_impl_list_map() : regular_impl_list_map();
 
     static const impl_list_item_t empty_list[] = {nullptr};
-    if (!p_impl_list) {
+
+    auto iter = map.find(dt_pair);
+    if (iter == map.end()) {
         dt_pair.dst_dt = data_type::undef;
-        p_impl_list = (const impl_list_map_t *)map[dt_pair];
-        if (!p_impl_list) return empty_list;
+        iter = map.find(dt_pair);
+        if (iter == map.end()) return empty_list;
     }
+
+    const impl_list_map_t *p_impl_list = (const impl_list_map_t *)iter->second;
 
     reorder_impl_key_t key {dt_pair.src_dt, dt_pair.dst_dt, src_md->ndims};
 
